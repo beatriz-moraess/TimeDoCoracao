01c0654cb0d94a055122f7f4e62bdbe848e3b1d3
Beatriz <beatriz.giovanna@gmail.com>
2025-03-23 14:26:13 -0300

    finaiza programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write ("S");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write ("a");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write ("n");
+
+Console.BackgroundColor= ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write ("t");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write ("o");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write ("s");
